06e2c2db73fdba1f8d987b07cc840db58331792a
Jonathan Strickland <djanatyn@gmail.com>
2022-02-21 13:12:19 -0500

    use elm2nix, working flake

--- flake.nix
+++ flake.nix
@@ -15,20 +15,29 @@
           smashgg-social-finder = pkgs.stdenv.mkDerivation {
             name = "smashgg-social-finder";
             version = "0.1.0";
-            src = ./src;
+            src = ./.;
 
-            buildInputs = with pkgs.elmPackages; [ elm ];
+            buildInputs = with pkgs.elmPackages; [
+              elm
+              pkgs.nodePackages.uglify-js
+            ];
 
-            buildPhase = ''
-              elm make $src/Main.elm --optimize --output=elm.js
-              # TODO: minify js (elm.js -> elm.min.js)
-            '';
+            buildPhase = pkgs.elmPackages.fetchElmDeps {
+              elmPackages = import ./elm-srcs.nix;
+              elmVersion = "0.19.1";
+              registryDat = ./registry.dat;
+            };
 
             installPhase = ''
-              mkdir -p $out
+              mkdir -p $out/share/doc
+
+              elm make src/Main.elm --output="$out/elm.js" --docs "$out/share/doc/smashgg.json"
+
+              uglifyjs $out/elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+                | uglifyjs --mangle --output $out/elm.min.js
 
               install -Dm755 index.html $out/index.html
-              install -Dm755 elm.min.js $out/elm.js
+              install -Dm755 style.css $out/style.css
             '';
           };
         });
