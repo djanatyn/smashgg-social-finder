3fe37c29b61843e0a356110b38a46b6f874726d7
Jonathan Strickland <djanatyn@gmail.com>
2022-02-21 13:14:40 -0500

    do not generate docs

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
             installPhase = ''
               mkdir -p $out/share/doc
 
-              elm make src/Main.elm --output="$out/elm.js" --docs "$out/share/doc/smashgg.json"
+              elm make src/Main.elm --output="$out/elm.js"
 
               uglifyjs $out/elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
                 | uglifyjs --mangle --output $out/elm.min.js
